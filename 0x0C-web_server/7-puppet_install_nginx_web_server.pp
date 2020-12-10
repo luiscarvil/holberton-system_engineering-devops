1ce3a9d10f7b97ce6a0ce2391b0ca87b02b37d59
luiscarvil <1888@holbertonschool.com>
2020-12-09 20:12:11 -0500

    puppet install nginx

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -8,13 +8,9 @@ file { 'index.html':
   content => 'Holberton School',
   path    => '/var/www/html/index.html'
 }
-file_line { 'append instruction':
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'server_name _;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-}
+exec {'/usr/bin/env sed -i "/listen 80 default_server;/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default': }
+
 service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+ensure  => running,
+require => Package['nginx'],
 }
