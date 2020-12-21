47cb74e0678570ffe51b0bebd36d84fdd996defe
luiscarvil <1888@holbertonschool.com>
2020-12-21 18:17:31 -0500

    puppet custom http

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -2,9 +2,9 @@
 # creating a custom HTTP header response, but with Puppet
 
 exec {'http header':
-command  => 'sudo apt update && 
-             sudo apt -y install nginx && 
-             sudo sed -i "/listen 80 default_server;/a\\\tadd_header X-Served-By \$hostname;\n" /etc/nginx/nginx.conf && 
+command  => 'sudo apt update;
+             sudo apt -y install nginx;
+             sudo sed -i "/server_name _/a \\\tadd_header X-Served-By \$hostname;\n" /etc/nginx/nginx.conf;
              sudo service nginx restart',
-provider => shell,
+provider => 'shell',
 }
