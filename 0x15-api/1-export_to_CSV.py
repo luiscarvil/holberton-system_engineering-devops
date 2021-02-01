3cfee7fe7adeabf01b6abb8566017819fb9c0698
luiscarvil <1888@holbertonschool.com>
2021-02-01 22:30:41 +0000

    send data from cvs file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import requests
+import json
+import csv
+from sys import argv
+
+if __name__ == "__main__":
+    url_todos = 'https://jsonplaceholder.typicode.com/todos'
+    url_users = 'https://jsonplaceholder.typicode.com/users'
+    employee_id = argv[1]
+
+    req_todos = requests.get(url_todos, params={'userId': employee_id}).json()
+    name_users = (requests.get(
+        url_users, params={'id': employee_id}).json())[0].get('name')
+    filename = employee_id + ".csv"
+    with open(filename, mode='w') as file_csv:
+        writer = csv.writer(file_csv, delimiter=',', quotechar='"',
+                            quoting=csv.QUOTE_ALL, lineterminator='\n')
+        for obj in req_todos:
+            if obj.get('userId') == int(employee_id):
+                writer.writerow([employee_id,
+                                name_users, str(obj.get('completed')),
+                                 obj.get('title')])
---
+++ 0x15-api/README.md
@@ -0,0 +1,1 @@
+create api, and rest- api python
