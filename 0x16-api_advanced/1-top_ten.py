497f6d6cda2ec5d077c70bb8ff2ca91ad3aaa574
luiscarvil <1888@holbertonschool.com>
2021-02-04 19:08:23 +0000

    add search api

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,14 +7,10 @@ import requests
 
 def top_ten(subreddit):
     url_req = requests.get("https://www.reddit.com/r/{}.json".format(
-        subreddit), headers={"User-Agent": "new_user_agent 1.0"}, allow_redirects=False)
-    if url_req.status_code != 200:
-        print(None)
-        return
-    """
-    for obj in url_req.json().get('data').get('children'):
-        print(obj.get('data', {}).get('title'))
-    """
+        subreddit), headers={"User-Agent": "new_user_agent 1.0"})
     # print((url_req.json().get('data').get('children'))[0].get('data').get('title'))
-    for obj in url_req.json().get('data').get('children')[:10]:
-        print(obj.get('data').get('title'))
+    try:
+        for obj in url_req.json().get('data').get('children')[:10]:
+            print(obj.get('data').get('title'))
+    except Exception:
+        print(None)
