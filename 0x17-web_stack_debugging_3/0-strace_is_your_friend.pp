6dd103957d94f82788ea6f06a1330ab7b99725a5
luiscarvil <1888@holbertonschool.com>
2021-02-18 09:14:47 -0500

    add task

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
 # fix error - replace extension .phpp to .php
-exec { 'puppet-lint':
-    command => 'sed -i "s/phpp/php/" /var/www/html/wp-settings.php',
+exec { 'fixing':
+    command => 'sudo sed -i "s/phpp/php/" /var/www/html/wp-settings.php',
     path    => ['/usr/bin', '/usr/sbin', '/usr/local/bin', '/usr/local/sbin'],
 }
-	
